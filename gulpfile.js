67cbc1d83ecfeff5869cac43a59a664da402a2ad
Artem Mevsha <aston4eg@gmail.com>
2015-03-03 12:19:30 +0200

    spritesmith add

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var nib = require('nib');
-var minifyCSS = require('gulp-minify-css');
-var jade = require('gulp-jade');
-var imagemin = require('gulp-imagemin');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename");
-var livereload = require('gulp-livereload');
-var connect = require('connect');
+var gulp        = require('gulp');
+var stylus      = require('gulp-stylus');
+var nib         = require('nib');
+var minifyCSS   = require('gulp-minify-css');
+var jade        = require('gulp-jade');
+var imagemin    = require('gulp-imagemin');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var rename      = require("gulp-rename");
+var livereload  = require('gulp-livereload');
+var connect     = require('connect');
 var serveStatic = require('serve-static');
 var spritesmith = require('gulp.spritesmith');
 
 gulp.task('stylus', function() {
-  gulp.src('./src/css/**/*.styl')
+  gulp.src('src/css/**/*.styl')
     .pipe(stylus({use: nib()}))
     .on('error', console.log)
-    .pipe(gulp.dest('./dest/css/'))
+    .pipe(gulp.dest('dest/css/'))
     .pipe(minifyCSS({keepBreaks:true}))
     .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest('./dest/css/'))
+    .pipe(gulp.dest('dest/css/'))
     .pipe(livereload());
 });
 
@@ -28,38 +28,38 @@ gulp.task('templates', function() {
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest('./dest/'))
+    .pipe(gulp.dest('dest/'))
     .pipe(livereload());
 });
 
 gulp.task('js', function() {
   return gulp.src(['src/js/**/*.js'])
-    .pipe(gulp.dest('./dest/js/'))
+    .pipe(gulp.dest('dest/js/'))
     .pipe(livereload());
 });
 
 gulp.task('imagemin',function(){
-   return gulp.src('./src/img/**/*')
+   return gulp.src('src/img/**/*')
       .pipe(imagemin())
-      .pipe(gulp.dest('./dest/img/'));
+      .pipe(gulp.dest('dest/img/'));
 });
 
 gulp.task('sprite', function() {
-    var spriteData =
-        gulp.src('./src/for_sprite/*.*') // путь, откуда берем картинки для спрайта
-            .pipe(spritesmith({
-                imgName: 'sprite.png',
-                cssName: 'sprite.styl',
-                cssFormat: 'stylus',
-                algorithm: 'binary-tree',
-                cssTemplate: 'stylus.template.mustache',
-                cssVarMap: function(sprite) {
-                    sprite.name = 's-' + sprite.name;
-                }
-            }));
+  var spriteData =
+    gulp.src('src/img/for_sprite/*.*') // путь, откуда берем картинки для спрайта
+      .pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.styl',
+        cssFormat: 'stylus',
+        algorithm: 'binary-tree',
+        cssTemplate: 'stylus.template.mustache',
+        cssVarMap: function(sprite) {
+          sprite.name = 's-' + sprite.name;
+        }
+      }));
 
-    spriteData.img.pipe(gulp.dest('./src/img/')); // путь, куда сохраняем картинку
-    spriteData.css.pipe(gulp.dest('./src/css/')); // путь, куда сохраняем стили
+  spriteData.img.pipe(gulp.dest('src/img/')); // путь, куда сохраняем картинку
+  spriteData.css.pipe(gulp.dest('src/css/')); // путь, куда сохраняем стили
 });
 
 gulp.task('server', function() {
@@ -74,27 +74,27 @@ gulp.task('server', function() {
 
 gulp.task('watch', function(){
   livereload.listen();
-  gulp.watch('./src/css/*.styl',['stylus']);
-  gulp.watch('./src/**/*.jade',['templates']);
-  gulp.watch('./src/img/**/*',['imagemin']);
-  gulp.watch('./src/js/**/*',['js']);
+  gulp.watch('src/css/*.styl',['stylus']);
+  gulp.watch('src/**/*.jade',['templates']);
+  gulp.watch('src/img/**/*',['imagemin']);
+  gulp.watch('src/js/**/*',['js']);
   gulp.start('server');
 });
 
 gulp.task('product', ['stylus','templates','imagemin','js'], function() {
-  gulp.src(['./dest/css/style.css',
-            './dest/css/media.css',
+  gulp.src(['dest/css/style.css',
+            'dest/css/media.css',
     ])
 		.pipe(concat('_main.min.css'))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./dest/css'));
+    .pipe(gulp.dest('dest/css'));
 
-  gulp.src(['./dest/js/vendor/jquery-1.11.0.min.js',
-            './dest/js/scripts.js',
+  gulp.src(['dest/js/vendor/jquery-1.11.0.min.js',
+            'dest/js/scripts.js',
     ])
     .pipe(uglify())
     .pipe(concat('_main.min.js'))
-    .pipe(gulp.dest('./dest/js'));
+    .pipe(gulp.dest('dest/js'));
 });
 
 
